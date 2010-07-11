3972fc871b3c1876a469ed77fbb2555ec013e7cd
Tomo <thomas.gambier@gmail.com>
2010-07-11 11:59:40 +0200

    FEATURE: WaitingActivity has a correct layout

--- src/net/homelinux/paubox/WaitingActivity.java
+++ src/net/homelinux/paubox/WaitingActivity.java
@@ -22,23 +22,8 @@ public class WaitingActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.waiting_layout);
+    	super.onCreate(savedInstanceState);
         
-        final OnClickListener radio_listener = new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-                // RadioButton rb = (RadioButton) v;
-                // Toast.makeText(CoincheActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
-            }
-        };
-        
-        Spinner s = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(
-                this, R.array.points, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-
-        
+    	setContentView(R.layout.waiting_layout);
     }
 }
